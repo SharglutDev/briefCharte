328ef424a8c361c629cfdffe79b6521445e45a5c
sharglut <sharglut@free.fr>
2022-09-12 10:39:11 +0200

    ajout du disabled sur random button

--- script.js
+++ script.js
@@ -1,54 +1,56 @@
 // Random Name Button Logic \\
 
 const studentList = [
-    'Paul',
-    'Floriane',
-    'Matthieu',
-    'Theo',
-    'Eric',
-    'Josselin',
-    'Philippe',
-    'Johann',
-    'Florian',
-    'Michael',
-    'Safae',
-    'Nicolas',
-    'Ludovic',
-    'Mouss',
-    'Rony',
-    'Etienne Etourdi',
-    'Etienne Entrainant',
-    'Soeli'
-]
+  "Paul",
+  "Floriane",
+  "Matthieu",
+  "Theo",
+  "Eric",
+  "Josselin",
+  "Philippe",
+  "Johann",
+  "Florian",
+  "Michael",
+  "Safae",
+  "Nicolas",
+  "Ludovic",
+  "Mouss",
+  "Rony",
+  "Etienne Etourdi",
+  "Etienne Entrainant",
+  "Soeli",
+];
 
-const randomButton = document.querySelector(".btn-random-name")
-const studentNumber = studentList.length
+const randomButton = document.querySelector(".btn-random-name");
+const studentNumber = studentList.length;
 
 const pickRandomStudent = () => {
-    const randomStudentNumber = Math.floor(Math.random() * studentNumber)
-    randomButton.innerHTML = `${studentList[randomStudentNumber]}`
-    setTimeout(() => {
-        randomButton.innerHTML = 'Random Apprenant'
-    }, 3000)
-}
+  const randomStudentNumber = Math.floor(Math.random() * studentNumber);
+  randomButton.innerHTML = `${studentList[randomStudentNumber]}`;
+  randomButton.setAttribute("disabled", "");
+  setTimeout(() => {
+    randomButton.innerHTML = "Random Apprenant";
+    randomButton.removeAttribute("disabled");
+  }, 3000);
+};
 
-randomButton.addEventListener('click', () => {
-    pickRandomStudent()
-})
+randomButton.addEventListener("click", () => {
+  pickRandomStudent();
+});
 
 // DROPDOWN BLOCK LOGIC \\
 
-const dropdownButtons = document.querySelectorAll(".dropdown")
+const dropdownButtons = document.querySelectorAll(".dropdown");
 
 const toggleClassActive = (clickedButton) => {
-    const parentNodex2 = clickedButton.parentNode.parentNode // selecting the first common parent between the dropdown button and the list
-    const childListItems = parentNodex2.querySelector(".list-items")
-    childListItems.classList.toggle('active')
-}
+  const parentNodex2 = clickedButton.parentNode.parentNode; // selecting the first common parent between the dropdown button and the list
+  const childListItems = parentNodex2.querySelector(".list-items");
+  childListItems.classList.toggle("active");
+};
 
 dropdownButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        toggleClassActive(button)
-        button.classList.toggle('rotate') // adding rotate(180deg) to the style worked only for 1 click
-    })
-})
+  button.addEventListener("click", () => {
+    toggleClassActive(button);
+    button.classList.toggle("rotate"); // adding rotate(180deg) to the style worked only for 1 click
+  });
+});
